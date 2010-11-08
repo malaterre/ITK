b8daaf0b584d4ed90d09967943a1502e568f4a35
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-08 23:53:53 +0100

    Add concrete implementation
    Change-Id: I0cd8a8251ed61af7944b5ffa20e0cdce50ff5eb1

---
+++ Code/IO/itkGDCMSCU.h
@@ -0,0 +1,49 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkGDCMSCU_h
+#define __itkGDCMSCU_h
+
+#include "itkDICOMSCU.h"
+
+#include <string>
+
+namespace itk
+{
+
+/** \class GDCMSCU
+ * \brief Abstract superclass defines image DICOM interface.
+ */
+class ITK_EXPORT GDCMSCU : public DICOMSCU
+{
+public:
+  /** Standard class typedefs. */
+  typedef GDCMSCU            Self;
+  typedef DICOMSCU           Superclass;
+  typedef SmartPointer<Self> Pointer;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(GDCMSCU, Superclass);
+
+private:
+  GDCMSCU(const Self&);      //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+};
+
+} // end namespace itk
+
+#endif // __itkGDCMSCU_h
