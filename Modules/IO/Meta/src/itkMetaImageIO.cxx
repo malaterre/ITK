7544cb2280133036fe401a4bf16544ecdf2d0b4b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2017-12-04 02:18:00 -0500

    ENH: Define a global default double precision for MetaImage
    This will allow a simple upgrade path from the legacy double precision
    of MetaImage to the new ones.
    
    Change-Id: If68f1f974b5447c609e28371333014a8d341a427

--- Modules/IO/Meta/src/itkMetaImageIO.cxx
+++ Modules/IO/Meta/src/itkMetaImageIO.cxx
@@ -24,6 +24,10 @@
 
 namespace itk
 {
+// Explicitly set std::numeric_limits<double>::max_digits10 this will provide
+// better accuracy when writing out floating point number in MetaImage header.
+unsigned int MetaImageIO::m_DefaultDoublePrecision = 17;
+
 MetaImageIO::MetaImageIO()
 {
   m_FileType = Binary;
@@ -42,6 +46,8 @@ MetaImageIO::MetaImageIO()
 
   this->AddSupportedReadExtension(".mha");
   this->AddSupportedReadExtension(".mhd");
+  // set behavior of MetaImageIO independently of the default value in MetaImage
+  this->SetDoublePrecision(GetDefaultDoublePrecision());
 }
 
 MetaImageIO::~MetaImageIO()
@@ -1263,4 +1269,15 @@ MetaImageIO::GetSplitRegionForWriting( unsigned int ithPiece,
 {
   return GetSplitRegionForWritingCanStreamWrite(ithPiece, numberOfActualSplits, pasteRegion);
 }
+
+void MetaImageIO::SetDefaultDoublePrecision(unsigned int precision)
+{
+  m_DefaultDoublePrecision = precision;
+}
+
+unsigned int MetaImageIO::GetDefaultDoublePrecision()
+{
+  return m_DefaultDoublePrecision;
+}
+
 } // end namespace itk
